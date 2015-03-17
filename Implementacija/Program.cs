30a85663baf2dfc6b013025e59993e629c0c43d0
bizmak <inveles@gmail.com>
2015-03-17 23:34:59 +0100

    prv commit

---
+++ Implementacija/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+interface IProba
+{
+    void Print();
+}
+
+class Proba : IProba
+{
+    public void Print()
+    {
+        Console.WriteLine("Proba interfejs pecatenje");
+        Console.ReadLine();
+    }
+}
+
+namespace Implementacija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proba P1 = new Proba();
+            P1.Print();
+        }
+    }
+}
